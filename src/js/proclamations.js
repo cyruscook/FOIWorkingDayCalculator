127fd7dcfca155bef3b6fd99c324c45eca8d35e7
EC2 Default User <ec2-user@ip-172-31-35-1.eu-west-1.compute.internal>
2023-10-15 12:45:03 +0000

    Set base href to current folder

--- src/js/proclamations.js
+++ src/js/proclamations.js
@@ -1,8 +1,8 @@
 import { DateTime } from "luxon";
 
 export const getProclamations = async function(api_origin) {
-    let req1 = fetch(`${api_origin}/bh_api/proclaimed_bhs.json`);
-    let req2 = fetch(`${api_origin}/bh_api/proclaimed_not_bhs.json`);
+    let req1 = fetch(`${api_origin}/proclaimed_bhs.json`);
+    let req2 = fetch(`${api_origin}/proclaimed_not_bhs.json`);
     let resp1 = (await req1).json();
     let resp2 = (await req2).json();
     const bhs = await resp1;
