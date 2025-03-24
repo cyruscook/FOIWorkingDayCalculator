8f0d27d8bc98e3a7b37167b22164b87fb76d11d1
cyruscook <11858105+cyruscook@users.noreply.github.com>
2025-03-24 23:37:47 +0000

    remove path prefix

--- src/js/proclaimed_dates.js
+++ src/js/proclaimed_dates.js
@@ -3,7 +3,7 @@ import { getProclamations } from './proclamations.js';
 
 let proclamations = undefined;
 try {
-    proclamations = await getProclamations(`https://d7rpp5pzwp0ap.cloudfront.net/bh_api`);
+    proclamations = await getProclamations(`https://d7rpp5pzwp0ap.cloudfront.net`);
 } catch (error) {
     console.error("Failed to fetch proclamations", error);
 }
