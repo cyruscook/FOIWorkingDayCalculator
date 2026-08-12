a7c152b0e254cfed18f9644a09450f8d17068e8a
cyruscook <11858105+cyruscook@users.noreply.github.com>
2026-08-12 18:44:36 +0100

    Handle new territory data

--- src/lib/proclamations.ts
+++ src/lib/proclamations.ts
@@ -4,6 +4,9 @@ import * as Collections from "typescript-collections";
 export type NoticesToDate = Collections.Dictionary<string, DateTime[]>;
 export type DateToNotices = Collections.Dictionary<DateTime, string[]>;
 
+type HolidayDatesByTerritory = Record<string, string[]>;
+type HolidayData = Record<string, HolidayDatesByTerritory>;
+
 export type Proclamations = {
   noticesToDate: {
     bhs: NoticesToDate;
@@ -17,13 +20,38 @@ export type Proclamations = {
 
 const api_origin = `https://d7rpp5pzwp0ap.cloudfront.net`;
 
+function strToDateTime(date: string): DateTime {
+  return DateTime.fromISO(date).setZone("Europe/London", {
+    keepLocalTime: true,
+  });
+}
+
+function addHolidayData(
+  holidayData: HolidayData,
+  noticesToDate: NoticesToDate,
+  dateToNotices: DateToNotices,
+): void {
+  for (const [notice, datesByTerritory] of Object.entries(holidayData)) {
+    const dates = [
+      ...new Set(Object.values(datesByTerritory).flat()),
+    ].map(strToDateTime);
+    noticesToDate.setValue(notice, dates);
+
+    for (const datetime of dates) {
+      const notices = dateToNotices.getValue(datetime) || [];
+      notices.push(notice);
+      dateToNotices.setValue(datetime, notices);
+    }
+  }
+}
+
 export const getProclamations = async function (): Promise<Proclamations> {
   const req1 = fetch(`${api_origin}/proclaimed_bhs.json`);
   const req2 = fetch(`${api_origin}/proclaimed_not_bhs.json`);
   const resp1 = (await req1).json();
   const resp2 = (await req2).json();
-  const bhs: Record<string, string[]> = await resp1;
-  const nbhs: Record<string, string[]> = await resp2;
+  const bhs: HolidayData = await resp1;
+  const nbhs: HolidayData = await resp2;
 
   const out = {
     noticesToDate: {
@@ -36,35 +64,8 @@ export const getProclamations = async function (): Promise<Proclamations> {
     },
   };
 
-  function strToDateTime(date: string): DateTime {
-    return DateTime.fromISO(date).setZone("Europe/London", {
-      keepLocalTime: true,
-    });
-  }
-
-  for (const key of Object.keys(bhs)) {
-    out.noticesToDate.bhs.setValue(key, bhs[key].map(strToDateTime));
-  }
-  for (const key of Object.keys(nbhs)) {
-    out.noticesToDate.nbhs.setValue(key, nbhs[key].map(strToDateTime));
-  }
-
-  for (const notice of Object.keys(bhs)) {
-    for (const date of bhs[notice]) {
-      const datetime = strToDateTime(date);
-      const notices = out.dateToNotices.bhs.getValue(datetime) || [];
-      notices.push(notice);
-      out.dateToNotices.bhs.setValue(datetime, notices);
-    }
-  }
-  for (const notice of Object.keys(nbhs)) {
-    for (const date of nbhs[notice]) {
-      const datetime = strToDateTime(date);
-      const notices = out.dateToNotices.nbhs.getValue(datetime) || [];
-      notices.push(notice);
-      out.dateToNotices.nbhs.setValue(datetime, notices);
-    }
-  }
+  addHolidayData(bhs, out.noticesToDate.bhs, out.dateToNotices.bhs);
+  addHolidayData(nbhs, out.noticesToDate.nbhs, out.dateToNotices.nbhs);
 
   return out;
 };
